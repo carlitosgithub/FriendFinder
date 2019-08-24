ac71795fe3f82919e3c1601499a0a81971d066b9
carlitos <cmmurillo1@hotmail.com>
2019-08-24 10:11:59 -0400

    files

---
+++ app/data/friend.js
@@ -0,0 +1,26 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Mario',
+    profilePic: '../../app/public/images/mario.jpg',
+    scores: ["4","5","5","2","4","5","4","4","3","4"]
+  },
+  {
+    name: 'Luigi',
+    profilePic: '../../app/public/images/luigi.jpg',
+    scores: ["1","3","5","5","4","5","3","4","3","1"]
+  },
+  {
+    name: 'Toad',
+    profilePic: '../../app/public/images/toad.png',
+    scores: ["2","4","4","3","5","1","5","5","4","5"]
+  },
+  {
+    name: 'Princess Peach',
+    profilePic: '../../app/public/images/toad.png',
+    scores: ["3","4","4","1","3","4","3","3","2","3"]
+  }
+];
+
+module.exports = friendList;
