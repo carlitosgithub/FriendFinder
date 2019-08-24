437879c67e210ada008bafdd17eb3169545d80e2
carlitos <cmmurillo1@hotmail.com>
2019-08-24 12:07:55 -0400

    pics

--- app/data/friend.js
+++ app/data/friend.js
@@ -3,22 +3,22 @@
 var friendList = [
   {
     name: 'Mario',
-    profilePic: '../../app/public/images/mario.jpg',
+    profilePic: '../public/images/mario.jpg',
     scores: ["4","5","5","2","4","5","4","4","3","4"]
   },
   {
     name: 'Luigi',
-    profilePic: '../../app/public/images/luigi.jpg',
+    profilePic: '../public/images/luigi.jpg',
     scores: ["1","3","5","5","4","5","3","4","3","1"]
   },
   {
     name: 'Toad',
-    profilePic: '../../app/public/images/toad.png',
+    profilePic: '../public/images/toad.jpg',
     scores: ["2","4","4","3","5","1","5","5","4","5"]
   },
   {
     name: 'Princess Peach',
-    profilePic: '../../app/public/images/toad.png',
+    profilePic: '../public/images/princess.jpg',
     scores: ["3","4","4","1","3","4","3","3","2","3"]
   }
 ];
